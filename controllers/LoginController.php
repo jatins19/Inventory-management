56510bc59b4682e18bf7fe41f73670a99bc62fe7
Jatin Sehgal <46214837+jatins19@users.noreply.github.com>
2026-07-07 11:41:21 +0530

    Add files via upload

---
+++ controllers/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Grocy\Controllers;
+
+use Grocy\Services\SessionService;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class LoginController extends BaseController
+{
+	public function LoginPage(Request $request, Response $response, array $args)
+	{
+		return $this->RenderPage($response, 'login');
+	}
+
+	public function Logout(Request $request, Response $response, array $args)
+	{
+		SessionService::GetInstance()->RemoveSession($_COOKIE[SessionService::SESSION_COOKIE_NAME]);
+		return $response->withRedirect($this->AppContainer->get('UrlManager')->ConstructUrl('/'));
+	}
+
+	public function ProcessLogin(Request $request, Response $response, array $args)
+	{
+		$authMiddlewareClass = GROCY_AUTH_CLASS;
+
+		$postParams = $request->getParsedBody();
+		if (isset($postParams['password_base64']))
+		{
+			$postParams['password'] = base64_decode($postParams['password_base64']);
+		}
+		unset($postParams['password_base64']);
+
+		if ($authMiddlewareClass::ProcessLogin($postParams))
+		{
+			return $response->withRedirect($this->AppContainer->get('UrlManager')->ConstructUrl('/'));
+		}
+		else
+		{
+			return $response->withRedirect($this->AppContainer->get('UrlManager')->ConstructUrl('/login?invalid=true'));
+		}
+	}
+}
